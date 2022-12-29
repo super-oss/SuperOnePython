5ac2453f02a40eee4babb998e36cf6dc90726d52
super-oss <64061753+super-oss@users.noreply.github.com>
2022-12-29 22:24:49 +0900

    Update new.py

--- main/hi/new.py
+++ main/hi/new.py
@@ -1,1 +1,6 @@
-#12345
+import csv, sys, os
+datalist = list(['aho', 'アホ', 'fool'], ['roman', '日本語', 'eng'])
+with open('downloads/test.csv', 'x', newline='') as csv1:
+    write = csv.writer(csv1, csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for newrow in datalist:
+        write.writerow(newrow)
